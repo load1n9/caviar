632f201815bc9dbb5b36ab6d5bc43259fb879647
loading <49134864+load1n9@users.noreply.github.com>
2022-02-15 11:02:08 -0500

    chore: updated dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/daybreak@v0.0.4/mod.ts";
+export * from "https://deno.land/x/daybreak@v0.0.6/mod.ts";
 export { Matrix4 } from "https://deno.land/x/gmath@0.1.11/mod.ts";
